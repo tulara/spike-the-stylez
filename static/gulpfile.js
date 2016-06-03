c8cc1dc3f66eedc898fc87e91b3b1f6ed2aa81e0
Tulara Webster <twebster@thoughtworks.com>
2016-06-03 22:02:03 +0100

    Minify the compiled sass file

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
 
-gulp.task('sass', function(){
+gulp.task('css', function(){
   return gulp.src('sass/all.scss')
     .pipe(sass())
+    .pipe(cssnano())
     .pipe(gulp.dest('dist/css'))
 });
