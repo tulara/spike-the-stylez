31155d1009a19669794d55afe27503747e92dc88
Tulara Webster <twebster@thoughtworks.com>
2016-06-03 22:08:49 +0100

    Put sass files with the rest of style guide assets

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 
 gulp.task('css', function(){
-  return gulp.src('sass/all.scss')
+  return gulp.src('assets/sass/all.scss')
     .pipe(sass())
     .pipe(cssnano())
     .pipe(gulp.dest('dist/css'))
